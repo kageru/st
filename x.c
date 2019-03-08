572da8708d717bae3e0af22e6c2352b825559513
kageru <kageru@encode.moe>
2019-03-08 22:00:50 +0100

    disable bold characters

--- x.c
+++ x.c
@@ -995,7 +995,7 @@ xloadfonts(char *fontstr, double fontsize)
 		die("st: can't open font %s\n", fontstr);
 
 	FcPatternDel(pattern, FC_WEIGHT);
-	FcPatternAddInteger(pattern, FC_WEIGHT, FC_WEIGHT_BOLD);
+	//FcPatternAddInteger(pattern, FC_WEIGHT, FC_WEIGHT_BOLD);
 	if (xloadfont(&dc.ibfont, pattern))
 		die("st: can't open font %s\n", fontstr);
 
